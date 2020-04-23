0f6a53e570aa601b879478ddaa093723a2e4b779
Alfredo Pabon <alfredrafael@gmail.com>
2020-04-23 16:40:28 -0400

    Finishes Media Queries for <About/>

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,7 +13,7 @@ return(
 <div className="about-info-area">
 <h2><strong>About<span> Alfredo Rafael</span></strong></h2>
         <img className="curve" src="https://alfredorafael.com/wp-content/uploads/2019/03/circlePicture.png" 
-        alt="A photograph of sliced kiwifruit on a while plate" />
+        alt="A photograph of Alfredo Rafael" />
         <p>Hi! I’m a US Army veteran, former langauge teacher and a <span>full stack developer</span>.
         I create <span>React.js</span>, jQuery, <span>Node.js</span> and <span>WordPress</span> applications for your business or organization. 
         My experience designing responsive and engaging user interfaces (<span>from wireframe to deployment</span>) and teamwork 
