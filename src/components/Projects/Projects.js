e25527542f7f5f6c3aceabe437c8312cec6c217a
Alfredo Pabon <alfredrafael@gmail.com>
2020-04-25 07:04:19 -0400

    more

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,70 +8,80 @@ const Projects = () => {
         <React.Fragment>
             <Container id="projects-component">
                 <h2> Pet Projects </h2>
-                <Fade>
-                    <Row>
-                        <Col>Description goes here</Col>
+                <Row>
+                    <Col>
+                        <div class="project-div" style={{flex: '1'}}>
+                            <img
+                                class="project-div-img"
+                                src="https://alfredorafael.com/wp-content/uploads/2020/04/movie-app.png"
+                                alt="project"
+                            />
+                            <div class="project-overlay-div">
+                                <div style={{ display: 'block' }}>
 
-                        <Col>
-                            <div class="project-div" style={{ margin: "0 auto" }}>
-                                <img
-                                    class="project-div-img"
-                                    src="https://alfredorafael.com/wp-content/uploads/2020/04/movie-app.png"
-                                    alt="project"
-                                />
-                                <div class="project-overlay-div">
-                                    <div style={{ display: 'block' }}>
-
-                                        <Button
-                                            className="overlay-content btn btn-outline-primary"
-                                            style={{
-                                                fontWeight: "thin",
-                                                color: "white",
-                                                fontSize: "100%",
-                                                padding: "0",
-                                            }}
-                                        >
-                                            Open Website
+                                    <Button
+                                        className="overlay-content btn btn-outline-primary"
+                                        style={{
+                                            fontWeight: "thin",
+                                            color: "white",
+                                            fontSize: "100%",
+                                            padding: "0",
+                                        }}
+                                    >
+                                        Open Website
                                         </Button>
 
 
-                                    </div>
-                                    <div style={{ display: 'block' }}>
+                                </div>
+                                <div style={{ display: 'block' }}>
 
 
-                                        <Button
-                                            className="overlay-content btn-outline-primary"
-                                            style={{
-                                                fontWeight: "thin",
-                                                color: "white",
-                                                fontSize: "100%",
-                                                padding: "0",
-                                            }}
-                                        >
+                                    <Button
+                                        className="overlay-content btn-outline-primary"
+                                        style={{
+                                            fontWeight: "thin",
+                                            color: "white",
+                                            fontSize: "100%",
+                                            padding: "0",
+                                        }}
+                                    >
 
-                                            <i className="devicon-github-plain"></i>
+                                        <i className="devicon-github-plain"></i>
                                         View Code
 
                                     </Button>
-                                    </div>
                                 </div>
                             </div>
-                        </Col>
-                    </Row>
-                </Fade>
+                        </div>
+
+                    </Col>
+
+                    <Col>
+                        <div style={{marginLeft: '-20%'}}>
+
+                            <h3>React.js Movie App</h3>
+
+                            <p>Bacon ipsum dolor amet shoulder pork chop buffalo, ham t-bone spare ribs leberkas turducken pork belly.
+                            Pastrami chislic short loin burgdoggen ball tip cow. Picanha drumstick sausage flank pork chop tail
+                            fatback bacon. Turkey turducken salami ball tip, corned beef pork loin porchetta burgdoggen tri-tip
+                            prosciutto meatloaf strip steak sausage buffalo pork belly.</p>
+                        </div>
+                    </Col>
+                </Row>
                 <Fade>
-                    <Row>
-                        <Col>Description goes here</Col>
-                        <Col>
-                            <div class="project-div">
-                                <img
-                                    class="project-div-img"
-                                    src="https://alfredorafael.com/wp-content/uploads/2019/05/NewsAPI.png"
-                                    alt="project"
-                                />
-                                <div class="project-overlay-div">
+                <Row>
+                    <Col>
+                        <div class="project-div" style={{flex: '1'}}>
+                            <img
+                                class="project-div-img"
+                                src="https://alfredorafael.com/wp-content/uploads/2020/04/movie-app.png"
+                                alt="project"
+                            />
+                            <div class="project-overlay-div">
+                                <div style={{ display: 'block' }}>
+
                                     <Button
-                                        className="overlay-content btn btn-outline-success"
+                                        className="overlay-content btn btn-outline-primary"
                                         style={{
                                             fontWeight: "thin",
                                             color: "white",
@@ -80,10 +90,15 @@ const Projects = () => {
                                         }}
                                     >
                                         Open Website
-                  </Button>
+                                        </Button>
+
+
+                                </div>
+                                <div style={{ display: 'block' }}>
+
 
                                     <Button
-                                        className="overlay-content btn btn-outline-success"
+                                        className="overlay-content btn-outline-primary"
                                         style={{
                                             fontWeight: "thin",
                                             color: "white",
@@ -91,26 +106,44 @@ const Projects = () => {
                                             padding: "0",
                                         }}
                                     >
+
+                                        <i className="devicon-github-plain"></i>
                                         View Code
-                  </Button>
+
+                                    </Button>
                                 </div>
                             </div>
-                        </Col>
-                    </Row>
+                        </div>
+
+                    </Col>
+
+                    <Col>
+                        <div style={{marginLeft: '-20%'}}>
+
+                            <h3>React.js Movie App</h3>
+
+                            <p>Bacon ipsum dolor amet shoulder pork chop buffalo, ham t-bone spare ribs leberkas turducken pork belly.
+                            Pastrami chislic short loin burgdoggen ball tip cow. Picanha drumstick sausage flank pork chop tail
+                            fatback bacon. Turkey turducken salami ball tip, corned beef pork loin porchetta burgdoggen tri-tip
+                            prosciutto meatloaf strip steak sausage buffalo pork belly.</p>
+                        </div>
+                    </Col>
+                </Row>
                 </Fade>
                 <Fade>
-                    <Row>
-                        <Col>Description goes here</Col>
-                        <Col>
-                            <div class="project-div" style={{ margin: "0 auto" }}>
-                                <img
-                                    class="project-div-img"
-                                    src="https://alfredorafael.com/wp-content/uploads/2019/05/ViTOuter.png"
-                                    alt="project"
-                                />
-                                <div class="project-overlay-div">
+                <Row>
+                    <Col>
+                        <div class="project-div" style={{flex: '1'}}>
+                            <img
+                                class="project-div-img"
+                                src="https://alfredorafael.com/wp-content/uploads/2020/04/movie-app.png"
+                                alt="project"
+                            />
+                            <div class="project-overlay-div">
+                                <div style={{ display: 'block' }}>
+
                                     <Button
-                                        className="overlay-content btn btn-outline-success"
+                                        className="overlay-content btn btn-outline-primary"
                                         style={{
                                             fontWeight: "thin",
                                             color: "white",
@@ -119,10 +152,15 @@ const Projects = () => {
                                         }}
                                     >
                                         Open Website
-                  </Button>
+                                        </Button>
+
+
+                                </div>
+                                <div style={{ display: 'block' }}>
+
 
                                     <Button
-                                        className="overlay-content btn btn-outline-success"
+                                        className="overlay-content btn-outline-primary"
                                         style={{
                                             fontWeight: "thin",
                                             color: "white",
@@ -130,15 +168,32 @@ const Projects = () => {
                                             padding: "0",
                                         }}
                                     >
+
+                                        <i className="devicon-github-plain"></i>
                                         View Code
-                  </Button>
+
+                                    </Button>
                                 </div>
                             </div>
-                        </Col>
-                    </Row>
+                        </div>
+
+                    </Col>
+
+                    <Col>
+                        <div style={{marginLeft: '-20%'}}>
+
+                            <h3>React.js Movie App</h3>
+
+                            <p>Bacon ipsum dolor amet shoulder pork chop buffalo, ham t-bone spare ribs leberkas turducken pork belly.
+                            Pastrami chislic short loin burgdoggen ball tip cow. Picanha drumstick sausage flank pork chop tail
+                            fatback bacon. Turkey turducken salami ball tip, corned beef pork loin porchetta burgdoggen tri-tip
+                            prosciutto meatloaf strip steak sausage buffalo pork belly.</p>
+                        </div>
+                    </Col>
+                </Row>
                 </Fade>
             </Container>
-        </React.Fragment>
+        </React.Fragment >
     );
 };
 
