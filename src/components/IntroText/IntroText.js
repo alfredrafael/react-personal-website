5d4ac5cf9f9bbba5ebb54aaa0d541247536c11be
alfredrafael <alfredrafael@gmail.com>
2020-06-26 10:59:04 -0400

    Adds typewritter effect to <IntroText/>

--- src/components/IntroText/IntroText.js
+++ src/components/IntroText/IntroText.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import './CssIntroText.css'; 
-
-
+import Typewriter from 'typewriter-effect';
+ 
 
 const IntroText = () => {
 return(
     <React.Fragment>
 
      <div className="carousel-overlay-text">
-		<div className="bg-transparent container">
-			<h1 className="display-4">Full-stack Developer</h1>
+		  <div className="bg-transparent container">
+      <h1 className="display-4">
+      {/*  Full-stack Developer  */}
+
+      <Typewriter
+      onInit={(typewriter) => {
+        typewriter.typeString('Full-Stack Developer')
+          .callFunction(() => {
+            console.log('String typed out!');
+          })
+          .pauseFor(1000)
+          .callFunction(() => {
+            console.log('All strings were deleted');
+          })
+          .start();
+      }}
+    />
+
+    </h1>
+
+
 	    </div>
     </div>
 
